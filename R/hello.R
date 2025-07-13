271c90e053540f841ac70eecf6079893c8077de3
malicmarijana <malic.marijana@gmail.com>
2025-07-13 15:19:56 +0200

    Initial commit

--- R/hello.R
+++ R/hello.R
@@ -30,3 +30,4 @@ usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
 usethis::use_git_remote("origin", url = "https://github.com/malicmarijana/ineqstats.git", overwrite = TRUE)
 devtools::document()
 
+devtools::document(roclets = c("namespace", "rd"))
